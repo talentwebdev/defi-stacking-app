22dfe41a81c65ade9e66afc346339040a6feadc3
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2021-12-20 17:16:30 -0400

    Deply smart contracts built so far.

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -1,8 +1,8 @@
-pragma solidity '^0.5.0';
+pragma solidity ^0.5.0;
 
 contract Migrations {
     address public owner;
-    uint public last_completed_migration;
+    uint256 public last_completed_migration;
 
     constructor() public {
         owner = msg.sender;
@@ -12,12 +12,12 @@ contract Migrations {
         if (msg.sender == owner) _; // contiue w/ function
     }
 
-    function set_completed(uint completed) public restricted {
+    function setCompleted(uint256 completed) public restricted {
         last_completed_migration = completed;
     }
 
     function upgrade(address new_address) public restricted {
         Migrations upgraded = Migrations(new_address);
-        upgrade.set_completed(last_completed_migration);
+        upgraded.setCompleted(last_completed_migration);
     }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -4,13 +4,13 @@ require('babel-polyfill');
 module.exports = {
     networks: {
         development: {
-            host: '127.0.0.1',
+            host: '127.0.0.1:7545',
             port: '7545',
             network_id:'*' // connect to any network
         }
     },
     contracts_directory: './src/contracts/',
-    contracts__build_directory: './src/truffle_abis',
+    contracts_build_directory: './src/truffle_abis',
     compilers: {
         solc: {
             version: '^0.5.0',
