014e83d583794a71b48c24d74b5e934288b1a486
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2021-12-21 06:00:17 -0400

    Created Tether and RWD and DecentralBank smart contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const Tether = artifacts.require('Tether');
+const RWD = artifacts.require('RWD');
+const DecentralBank = artifacts.require('DecentralBank');
 
 module.exports = async function(deployer) {
     await deployer.deploy(Tether);
+
+    await deployer.deploy(RWD);
+
+    await deployer.deploy(DecentralBank);
 };
