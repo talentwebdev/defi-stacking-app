d715f7d6fc1d257bab17af58e9e78835c3a1a4fa
TalentWebDev <49565243+talentwebdev@users.noreply.github.com>
2022-01-12 01:38:28 -0400

    refactored tests.

--- test/decentralBank.test.js
+++ test/decentralBank.test.js
@@ -5,12 +5,26 @@ const DecentralBank = artifacts.require('DecentralBank');
 require('chai').use(require('chai-as-promised')).should();
 
 contract('DecentralBank', (accounts) => {
+    let tether, rwd;
+
+    before(async () => {
+        tether = await Tether.new();
+        rwd = await RWD.new();
+    })
+
     describe('Mock Tether Deployment', async () => {
         it('matches name successfully', async () => {
-            let tether = await Tether.new();
             const name = await tether.name();
             
             assert.equal(name, 'Tether');
+        });
+    });
+
+    describe('Reward Token', async () => {
+        it('matches name successfully', async () => {
+            const name = await rwd.name();
+            
+            assert.equal(name, 'Reward Token');
         });
     });
 });
